ff8ada2f141872e990055498c517284b2d3db9a9
Charles Max Wood <chuck@devchat.tv>
2015-03-09 17:35:17 -0600

    Move routing logic to its own file

--- config.ru
+++ config.ru
@@ -1,48 +1,6 @@
 require 'rubygems'
 require 'rack'
-
-run Proc.new { |env| ['404', {'Content-Type' => 'text/html'}, ['Sorry, can\'t find your stuff']] }
-
-def get(path, &block)
-  app = Class.new do
-    @path = path
-    @block = block
-
-    class << self
-      attr_reader :path, :block
-    end
-
-    def initialize(app)
-      @app = app
-    end
-
-    def path
-      self.class.path
-    end
-
-    def block
-      self.class.block
-    end
-
-    def request
-      Rack::Request.new(@env)
-    end
-
-    def params
-      request.params
-    end
-
-    def call(env)
-      @env = env
-      if request.request_method == 'GET' && request.path_info == path
-        instance_eval(&block)
-      else
-        @app.call(env)
-      end
-    end
-  end
-  use app
-end
+require_relative 'lib/sinatra_route'
 
 get '/' do
   ['200', {'Content-Type' => 'text/html'}, ["#{params}"]]
@@ -51,3 +9,5 @@ end
 get '/fart' do
   ['200', {'Content-Type' => 'text/html'}, ["#{request.request_method}"]]
 end
+
+run Proc.new { |env| ['404', {'Content-Type' => 'text/html'}, ['Sorry, can\'t find your stuff']] }
---
+++ lib/sinatra_route.rb
@@ -0,0 +1,78 @@
+class SinatraRoute
+  class << self
+    attr_reader :request_method, :path, :block
+  end
+
+  def initialize(app)
+    @app = app
+  end
+
+  def path
+    request.path_info
+  end
+
+  def request_method
+    request.request_method
+  end
+
+  def _path
+    self.class.path
+  end
+
+  def _block
+    self.class.block
+  end
+
+  def _request_method
+    self.class.request_method
+  end
+
+  def request
+    Rack::Request.new(@env)
+  end
+
+  def params
+    request.params
+  end
+
+  def call(env)
+    @env = env
+    if request_method == _request_method && path == _path
+      instance_eval(&_block)
+    else
+      @app.call(env)
+    end
+  end
+end
+
+def SinatraRoute(request_method, path, block)
+  Class.new(SinatraRoute) do
+    @request_method = request_method
+    @path = path
+    @block = block
+  end
+end
+
+def get(path, &block)
+  use SinatraRoute('GET', path, block)
+end
+
+def post(path, &block)
+  use SinatraRoute('POST', path, block)
+end
+
+def put(path, &block)
+  use SinatraRoute('PUT', path, block)
+end
+
+def patch(path, &block)
+  use SinatraRoute('PATCH', path, block)
+end
+
+def delete(path, &block)
+  use SinatraRoute('DELETE', path, block)
+end
+
+def options(path, &block)
+  use SinatraRoute('OPTIONS', path, block)
+end
